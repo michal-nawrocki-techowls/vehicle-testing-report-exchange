c822c37b5a2b64ca8bf91768e00960ef65fb1380
Adam Orendorz <aorendorz@gmail.com>
2021-04-08 22:03:05 +0200

    Refactoring XSD

--- app/vix/src/main/java/wsdl/vix/VixEndpoint.java
+++ app/vix/src/main/java/wsdl/vix/VixEndpoint.java
@@ -15,34 +15,19 @@ import java.util.TimeZone;
 @Endpoint
 public class VixEndpoint {
 
-    @PayloadRoot(namespace = VixConfig.VIX_NAMESPACE, localPart = "LoginRequest")
-    @ResponsePayload
-    public LoginResponse login(@RequestPayload LoginRequest request) throws DatatypeConfigurationException {
-        ObjectFactory f = new ObjectFactory();
-        LoginResponse response = f.createLoginResponse();
-        LoginStatus status = f.createLoginStatus();
-        status.setStatus(Status.OK);
-        status.setSession(request.getToken());
-        GregorianCalendar gc = new GregorianCalendar();
-        gc.setTimeZone(TimeZone.getTimeZone("UTC"));
-        status.setExpiryDateTime(DatatypeFactory.newInstance().newXMLGregorianCalendar(gc));
-        response.setLoginStatus(status);
-        return response;
-    }
-
     @PayloadRoot(namespace = VixConfig.VIX_NAMESPACE, localPart = "DocumentRequest")
     @ResponsePayload
     public DocumentResponse document(@RequestPayload DocumentRequest request) throws DatatypeConfigurationException {
         ObjectFactory f = new ObjectFactory();
         DocumentResponse response = f.createDocumentResponse();
         Document document = f.createDocument();
+        DocumentExt ext = new DocumentExt();
 
-       Document.Fields fields = new Document.Fields();
-        Document.Fields.Field field = new Document.Fields.Field();
-       fields.getField().add(field);
+        DocumentListResponse resp = new DocumentListResponse();
 
-
-        response.setDocument(document);
+        ext.setTitle("Hello");
+        ext.setExtra("Extra");
+        response.setDocument((Document) ext);
         return response;
     }
 
